6410948b98df5ad638c0d511f9564795134f07b0
Liam Wang <5054950+wangl5@users.noreply.github.com>
2022-12-03 11:45:20 -0500

    Refactor dl

--- download_data.py
+++ download_data.py
@@ -4,15 +4,6 @@ import zipfile
 import shutil
 import os
 
-aligned_test_url = "https://www.ugrad.cs.jhu.edu/~dlu17/aligned_test.zip"
-aligned_train_url = "https://www.ugrad.cs.jhu.edu/~dlu17/aligned_train.zip"
-list_partition_label_url = "https://www.ugrad.cs.jhu.edu/~dlu17/list_patition_label.txt"
-manual_url = "https://www.ugrad.cs.jhu.edu/~wwang136/manual.zip"
-
-rafdb = Path('./data/RAF')
-rafdb.mkdir(exist_ok=True, parents=True)
-
-# download and unzip the data using python
 
 def download_and_unzip(url, dest):
     # if already exists, return
@@ -35,12 +26,25 @@ def remove_parent_dir(target):
     shutil.rmtree("tmp")
 
 
-download_and_unzip(aligned_test_url, rafdb / 'aligned_test.zip')
-download_and_unzip(aligned_train_url, rafdb / 'aligned_train.zip')
-download_and_unzip(manual_url, rafdb / 'manual.zip')
+def download_raf():
+    aligned_test_url = "https://www.ugrad.cs.jhu.edu/~dlu17/aligned_test.zip"
+    aligned_train_url = "https://www.ugrad.cs.jhu.edu/~dlu17/aligned_train.zip"
+    list_partition_label_url = "https://www.ugrad.cs.jhu.edu/~dlu17/list_patition_label.txt"
+    manual_url = "https://www.ugrad.cs.jhu.edu/~wwang136/manual.zip"
 
-remove_parent_dir(rafdb / 'aligned_test')
-remove_parent_dir(rafdb / 'aligned_train')
+    rafdb = Path('./data/RAF')
+    rafdb.mkdir(exist_ok=True, parents=True)
 
-# download the list_patition_label.txt file
-urllib.request.urlretrieve(list_partition_label_url, rafdb / 'list_partition_label.txt')
+    download_and_unzip(aligned_test_url, rafdb / 'aligned_test.zip')
+    download_and_unzip(aligned_train_url, rafdb / 'aligned_train.zip')
+    download_and_unzip(manual_url, rafdb / 'manual.zip')
+
+    remove_parent_dir(rafdb / 'aligned_test')
+    remove_parent_dir(rafdb / 'aligned_train')
+
+    # download the list_patition_label.txt file
+    urllib.request.urlretrieve(list_partition_label_url, rafdb / 'list_partition_label.txt')
+
+
+if __name__ == '__main__':
+    download_raf()
